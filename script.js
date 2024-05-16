991d2d3e573f5c1d66659e343722a775f48b39ed
Gowthamimaidal <gowthamimaydal@gmail.com>
2024-05-16 14:54:25 +0530

    j

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    .addEventListener('submit',Function(e) {
-
-    
-
-    // Simple validation
+*document.getElementById('contact-form')
+.addEventListener('submit', function(e) {
     e.preventDefault();
-    var name = document.getElementById('name').value;
+    var name = document.getElementById('name').value; 
     var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
     var message = document.getElementById('message').value;
-    var subject = "New message from " + name;
-    var body = "Name :" + name + "\nEmail:" + email + "\nMessage:" + message;
-    var mailto_link = 'mailto : gowthamimaydal@gmail.com' + '?subject=' + subject +'&body=' + encodeURIComponent(body);
+    var subject = "New message from " + name; 
+    var body = "Name: " + name + "\nEmail: " + email + "\nMessage: " + message;
+    var mailto_link = 'mailto:Its your mail id' + '?subject=' 
+    + subject + '&body=' + encodeURIComponent(body);
     window.location.href = mailto_link;
-
-    
-});
+  });
